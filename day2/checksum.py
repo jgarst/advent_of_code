32e9476185dd8de2200b100ebac64c285a8196fe
Jared Hall Garst <jared.h.garst@gmail.com>
2018-02-28 00:21:50 -0800

    day2: pass pylint

--- day2/checksum.py
+++ day2/checksum.py
@@ -7,7 +7,7 @@ from itertools import combinations
 
 def minmax(row: List[int]) -> Tuple[int, int]:
     """Minimum and maximum from a list of integers."""
-    if len(row) == 0:
+    if not row:
         raise ValueError('A length zero list has no minimum or maximum')
 
     mymin = mymax = row[0]
@@ -22,7 +22,7 @@ def minmax(row: List[int]) -> Tuple[int, int]:
 
 def divisors(row: List[int]) -> Tuple[int, int]:
     """Sorted first two evenly divisible values from a list of integers."""
-    if len(row) == 0:
+    if not row:
         raise ValueError('A length zero list has no evenly divisble values')
 
     # don't think there is a way to do this under n^2 without more information
@@ -33,25 +33,31 @@ def divisors(row: List[int]) -> Tuple[int, int]:
     raise ValueError('list has no evenly divisible values')
 
 
-def divisiblesum(spreadsheet: List[List[int]]) -> int:
+def divisiblesum(rows: List[List[int]]) -> int:
     """Sum the division of evenly divisble values from integer lists."""
     return sum(
         numerator // denominator for denominator, numerator in
-        (divisors(row) for row in spreadsheet)
+        (divisors(row) for row in rows)
     )
 
 
-def checksum(spreadsheet: List[List[int]]) -> int:
+def checksum(rows: List[List[int]]) -> int:
     """Sum the maximum absolute differences of integer lists."""
     return sum(
         row_max - row_min for row_min, row_max in
-        (minmax(row) for row in spreadsheet)
+        (minmax(row) for row in rows)
     )
 
 
-if __name__ == "__main__":
-    with open('input') as input:
-        input = [[int(i) for i in line.split()] for line in input]
+def main():
+    """Run 'input' against day 2 challenges."""
+    with open('input') as input_file:
+        spreadsheet = [[int(i) for i in line.split()] for line in input_file]
 
-    print(checksum(input))
-    print(divisiblesum(input))
+    print(spreadsheet)
+    print(checksum(spreadsheet))
+    print(divisiblesum(spreadsheet))
+
+
+if __name__ == "__main__":
+    main()
